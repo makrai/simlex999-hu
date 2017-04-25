3fe8e96cb50b2da1d951cc2f896d9aebc4ac29b2
Márton Makrai <makrai.nlp@gmail.com>
2017-04-25 13:02:05 +0200

    code refactored

--- translate_sl999.py
+++ translate_sl999.py
@@ -11,86 +11,15 @@ class SimLex999Translator():
     # What if a word would be disambiguated differently in different pairs
     # containing it?
     def __init__(self):
-        lg_fm = "%(asctime)s %(module)s (%(lineno)s) %(levelname)s %(message)s"
+        lg_fm = "%(module)s (%(lineno)s) %(levelname)s %(message)s"
+        "%(asctime)s"
         self.parse_args()
-        logging.basicConfig(format=lg_fm, level=logging.INFO,
-                            filename=self.args.log_filen, filemode='w')
-
-    def main(self):
+        logging.basicConfig(format=lg_fm, level=logging.INFO)
+        #, filename=self.args.log_filen, filemode='w')
         self.dicts = [self.read_dict(fn) for fn in self.args.dicts]
         self.embed = self.get_embed(self.args.embed)
         self.en_hu = defaultdict(set)
-        # en_hu is for checking whether multiple occurrences of a word are
-        # disambiguated the same way
-        with open(self.args.simlex) as f, open(self.args.output_filen,
-                                               mode='w') as out_f:
-            logging.info('reading {}, writing to {}'.format(
-                self.args.simlex, self.args.output_filen))
-            for line in f:
-                en1, en2, tail = line.strip().split('\t', 2)
-                logging.info((en1, en2))
-                if en1 == 'word1':
-                    logging.info('skipping header {}'.format(line))
-                    continue
-                try:
-                    hus1, hus2 = [self.translate(word) for word in [en1, en2]]
-                except OOVException as e:
-                    logging.warn(e)
-                    continue                    
-                intersection = hus1.intersection(hus2)
-                len_intersection = len(intersection)
-                if intersection and len_intersection == 2: 
-                    hu1, hu2 = intersection
-                else:
-                    if not intersection:
-                        hus1, hus2 = (list(set_) for set_ in [hus1, hus2])
-                    elif len_intersection == 1:
-                        hus1, hus2 = (list(intersection),
-                                      list(hus1.symmetric_difference(hus2)))
-                    elif len_intersection > 2:
-                        raise logging.warn('Three synonyms: {}'.intersection)
-                    try:
-                        mx1, mx2 = (self.words2mx(words) 
-                                    for words in [hus1, hus2])
-                    except OOVException as e:
-                        logging.warn(e)
-                        continue
-                    w1_ids, w2_ids = zip(*self.argclosest(mx1, mx2))
-                    logging.debug(np.array(hus1)[np.array(w1_ids)])
-                    logging.debug(np.array(hus2)[np.array(w2_ids)])
-                    hu1, hu2 = hus1[w1_ids[0]], hus2[w2_ids[0]]
-                self.en_hu[en1].add(hu1)
-                self.en_hu[en2].add(hu2)
-                logging.info((hu1, hu2))
-                #out_f.write('{}\t{}\t{}\n'.format(hu1, hu2, tail))
-                out_f.write('{}\t{}\t{}\t{}\t{}\n'.format(en1, en2, hu1, hu2,
-                                                          len_intersection))
-        for en in sorted(self.en_hu, key=lambda en: len(self.en_hu[en]), reverse=True):
-            hus = self.en_hu[en]
-            if len(hus) == 1:
-                break
-            else:
-                logging.debug((en, hus))
-
-    def argclosest(self, mx1, mx2):
-        product = mx1.dot(mx2.T)
-        elems0 = np.argsort(-product, axis=None)
-        return [np.unravel_index(elem0, product.shape) for elem0 in elems0]
-
-    def translate(self, word):
-        for dict_ in self.dicts:
-            if word in dict_:
-                return dict_[word]
-        raise(OOVException('oov: {} {}'.format(word, len(word))))
-
-    def words2mx(self, words):
-        mx = np.asarray([self.embed[word] 
-                         for word in words if word in self.embed])
-        if len(mx):
-            return mx
-        else:
-            raise OOVException('none of words {} found in embedding'.format(
-                ' '.join(words)))
+        self.oov_dict, self.oov_embed, self.oov_synon = set(), set(), set()
 
     def parse_args(self):
         parser = argparse.ArgumentParser(
@@ -105,11 +34,15 @@ class SimLex999Translator():
         parser.add_argument(
             '--simlex', help='input word similarity dataset',
             default = '/mnt/permanent/Language/English/Data/SimLex-999/SimLex-999.txt')
-        parser.add_argument('dicts', nargs='+', help="dictionary tsv's in order \
-                            of reliability")
+        parser.add_argument(
+            '--dicts', nargs='+', help="dictionary tsv's in order of reliability",
+            default=['/mnt/store/makrai/data/language/hungarian/dict/wikt2dict-en-hu'])
         parser.add_argument(
             '--embed', help='without extension',
             default='/mnt/permanent/Language/Hungarian/Embed/webkorp/word2vec_n5_152_m50_w10_0l_0#')
+        parser.add_argument('--oov_dict', default='oov_dict.log')
+        parser.add_argument('--oov_embed', default='oov_embed.log')
+        parser.add_argument('--oov_synon', default='oov_synon.log')
             #webkorp/glove-hu_152')
             #webkorp/webkorp_d600_w12_m250_cbow_h0_n7_i12')
             #webkorp/webkorp_d600_w12_m250_cbow_h0_n7_i12')
@@ -118,6 +51,83 @@ class SimLex999Translator():
         parser.add_argument('--output_filen', default='out.tsd')
         parser.add_argument('--log_filen', default='log.log')
         self.args = parser.parse_args()
+
+    def main(self):
+        with open(self.args.simlex) as simlex_f, open(
+                self.args.output_filen, mode='w') as out_f:
+            logging.info('reading {}, writing to {}'.format(
+                self.args.simlex, self.args.output_filen))
+            for line in simlex_f:
+                en1, en2, _pos, sim, tail = line.strip().split('\t', 4)
+                logging.debug((en1, en2))
+                if en1 == 'word1':
+                    logging.info('skipping header {}'.format(line))
+                    continue
+                try:
+                    hus1, hus2 = [self.translate(word) for word in [en1, en2]]
+                    hu1, hu2, len_intersection = self.disambig(en1, en2, hus1,
+                                                               hus2, sim)
+                except OOVException as e:
+                    logging.debug((e, en1, en2, hus1, hus2))
+                    continue                    
+                self.en_hu[en1].add(hu1)
+                self.en_hu[en2].add(hu2)
+                logging.debug((hu1, hu2))
+                out_f.write('{}\t{}\t{}\t{}\t{}\n'.format(
+                    en1, en2, hu1, hu2, len_intersection))
+        self.logg_ambig()
+        self.logg_oov(self.oov_dict, self.args.oov_dict)
+        self.logg_oov(self.oov_embed, self.args.oov_embed)
+        self.logg_oov(self.oov_synon, self.args.oov_synon)
+
+    def translate(self, word):
+        for dict_ in self.dicts:
+            if word in dict_:
+                return dict_[word]
+        self.oov_dict.add(word)
+        raise(OOVException('not in dictionary: {}'.format(word)))
+
+    def disambig(self, en1, en2, hus1, hus2, sim):
+        # Different disambiguation strategy is followed depending on the size
+        # of the intersection of the dictionary-provided translations of the
+        # two words in the similarity pair.
+        intersection = hus1.intersection(hus2)
+        len_intersection = len(intersection)
+        if len_intersection == 2: 
+            return list(intersection) + [len_intersection]
+        elif len_intersection == 0:
+            hus1, hus2 = (list(set_) for set_ in [hus1, hus2])
+        elif len_intersection == 1:
+            hus1, hus2 = (list(intersection),
+                          list(hus1.symmetric_difference(hus2)))
+            if not hus2:
+                self.oov_synon.add((en1, en2)) 
+                raise OOVException(
+                    'The only translation for both {} is {}'.format(
+                        (en1, en2), hus1))
+        elif len_intersection > 2:
+            raise NotImplementedError('Three synonyms: {}'.intersection)
+        mx1, mx2 = (self.words2mx(words) for words in [hus1, hus2])
+        w1_ids, w2_ids = zip(*self.argclosest(mx1, mx2, sim))
+        logging.debug(np.array(hus1)[np.array(w1_ids)])
+        logging.debug(np.array(hus2)[np.array(w2_ids)])
+        return hus1[w1_ids[0]], hus2[w2_ids[0]], len_intersection
+
+    def words2mx(self, words):
+        mx = np.asarray([self.embed[word] 
+                         for word in words if word in self.embed])
+        if len(mx):
+            return mx
+        else:
+            for word in words:
+                self.oov_embed.add(word) 
+            raise OOVException('not in embedding: {}'.format(
+                ' '.join(words)))
+
+    def argclosest(self, mx1, mx2, sim=10):
+        product = np.absolute(mx1.dot(mx2.T) - float(sim)/10)
+        elems0 = np.argsort(product, axis=None)
+        return [np.unravel_index(elem0, product.shape) for elem0 in elems0]
 
     def read_dict(self, dict_fn):
         dict_ = defaultdict(set)
@@ -137,6 +147,21 @@ class SimLex999Translator():
             embed = KeyedVectors.load_word2vec_format('{}.w2v'.format(filen))
             embed.save(gens_fn)
             return embed
+
+    def logg_ambig(self):
+        """
+        This function logs the translations we used if there are more.
+        """
+        for en, hus in self.en_hu.items():
+            # sorted(self.en_hu, key=lambda en: len(self.en_hu[en]),
+            # reverse=True):
+            if len(hus) != 1:
+                logging.debug((en, hus))
+
+    def logg_oov(self, set_, filen):
+        with open(filen, mode='w') as log_f:
+            for item in set_:
+                log_f.write('{}\n'.format(item))
 
 class OOVException(Exception):
     pass
